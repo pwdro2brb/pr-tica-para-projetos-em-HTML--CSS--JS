153df584b071132c06e5ef62cfdaf4fb9587bd2f
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-10-31 23:08:41 -0300

    Script do projeto 1 começo

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
         $('ul').toggleClass('show')
     })
 })
+
+let igual_apertado = 0
+//refere-se a todos os botões exceto AC e DEL
+let buttom_input = document.querySelector("botao-input")
+//Referência o valores para input, igual, limpar tudo, apagar
+
+let input = document.getElementById("input")
+let igual = document.getElementById("igual")
+let limpar = document.getElementById("apagar")
+let apagar = document.getElementById("apaga")
+
+window.onload = () => {
+    input.value = ""
+}
+
