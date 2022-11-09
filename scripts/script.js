c64e7cb095a2f91552c7084e3e3b73f3d7a43d9c
Pedro Henrique Soares <phenriquesoares360@gmail.com>
2022-11-09 16:13:51 -0300

    restruturação calculadora Oriundo de que a anterior não funcionava

--- scripts/script.js
+++ scripts/script.js
@@ -4,18 +4,3 @@ $(document).ready(function(){
         $('div').toggleClass('esconda')
     })
 })
-
-let igual_apertado = 0
-//refere-se a todos os botões exceto AC e DEL
-let buttom_input = document.querySelector("botao-input")
-//Referência o valores para input, igual, limpar tudo, apagar
-
-let input = document.getElementById("input")
-let igual = document.getElementById("igual")
-let limpar = document.getElementById("apagar")
-let apagar = document.getElementById("apaga")
-
-window.onload = () => {
-    input.value = ""
-}
-
